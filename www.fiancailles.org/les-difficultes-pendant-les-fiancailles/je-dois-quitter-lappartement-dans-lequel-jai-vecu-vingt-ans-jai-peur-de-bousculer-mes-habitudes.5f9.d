b68a4789af24e9624180e0e44af87ae2d12fddaa
Emmanuel DE SAINT STEBAN <emmanuel.desaintsteban@evidian.com>
2021-02-17 22:09:36 +0100

    Fix url and naming files

--- www.fiancailles.org/les-difficultes-pendant-les-fiancailles/je-dois-quitter-lappartement-dans-lequel-jai-vecu-vingt-ans-jai-peur-de-bousculer-mes-habitudes.5f9.d
+++ www.fiancailles.org/les-difficultes-pendant-les-fiancailles/je-dois-quitter-lappartement-dans-lequel-jai-vecu-vingt-ans-jai-peur-de-bousculer-mes-habitudes.5f9.d
@@ -288,7 +288,7 @@ ga('set', {
       (function(d){
       var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
       js = d.createElement('script'); js.id = id; js.async = true;
-      js.src = '../../connect.facebook.net/fr_FR/all.js';
+      js.src = 'https://connect.facebook.net/fr_FR/all.js';
       d.getElementsByTagName('head')[0].appendChild(js);
     }(document));
 
